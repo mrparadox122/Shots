87bf8695212ea024bfa07d3ec8855927b2bc457f
mohan <mohantarabindra140@gmail.com>
2022-01-12 17:31:24 +0530

    7.8

--- app/src/main/java/com/paradox/projectsp3/MainRecyclerView/VideoPlayerViewHolder.java
+++ app/src/main/java/com/paradox/projectsp3/MainRecyclerView/VideoPlayerViewHolder.java
@@ -4,24 +4,44 @@ package com.paradox.projectsp3.MainRecyclerView;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.net.Uri;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 import com.bumptech.glide.RequestManager;
+import com.paradox.projectsp3.AccountActivity;
+import com.paradox.projectsp3.Comment_fragment;
+import com.paradox.projectsp3.MainActivity;
 import com.paradox.projectsp3.Model.MediaObject;
 import com.paradox.projectsp3.R;
+
+import java.io.File;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class VideoPlayerViewHolder extends RecyclerView.ViewHolder {
 
+
+      Fragment fragment;
+      FragmentManager fragmentManager;
+      FragmentTransaction fragmentTransaction;
 
 
     FrameLayout media_container;
@@ -32,6 +52,7 @@ public class VideoPlayerViewHolder extends RecyclerView.ViewHolder {
     ProgressBar progressBar;
     View parent;
     RequestManager requestManager;
+    LinearLayout CommentView;
     boolean checklike=true;
 
 
@@ -43,9 +64,8 @@ public class VideoPlayerViewHolder extends RecyclerView.ViewHolder {
     TextView commentn;
 
 
-
-
     TextView share;
+    Context context;
 
 
 
@@ -62,18 +82,31 @@ public class VideoPlayerViewHolder extends RecyclerView.ViewHolder {
         like=itemView.findViewById(R.id.imageView9);
         likesn = itemView.findViewById(R.id.likesn);
         commentn = itemView.findViewById(R.id.comments);
-        Share=itemView.findViewById(R.id.imageView6);
+
         Comment=itemView.findViewById(R.id.imageView8);
-
-
-
-
-
-
-
+       context= itemView.getContext();
+        Share=itemView.findViewById(R.id.imageView6);
+        CommentView=itemView.findViewById(R.id.commentView);
         share = itemView.findViewById(R.id.share);
 
 
+
+
+       Comment.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               soundDisk.setVisibility(View.GONE);
+               //Toast.makeText(context.getApplicationContext(), "ggggg",Toast.LENGTH_SHORT).show();
+             CommentView.setVisibility(View.VISIBLE);
+               fragment=new Comment_fragment();
+               fragmentManager=((FragmentActivity)context).getSupportFragmentManager();
+               fragmentTransaction=fragmentManager.beginTransaction();
+               fragmentTransaction.replace(R.id.commentView,fragment);
+               fragmentTransaction.commit();
+
+
+           }
+       });
 
 
 
@@ -99,7 +132,17 @@ public class VideoPlayerViewHolder extends RecyclerView.ViewHolder {
         Share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent in
+
+                Intent myIntent = new Intent(Intent.ACTION_SEND);
+                myIntent.setType("video/mp4");
+                String body = "Your body here";
+                String sub = "Your Subject";
+                myIntent.putExtra(Intent.EXTRA_SUBJECT,sub);
+                myIntent.putExtra(Intent.EXTRA_TEXT,body);
+                context.startActivity(Intent.createChooser(myIntent, "Share Using"));
+
+
+
             }
         });
 
@@ -108,6 +151,8 @@ public class VideoPlayerViewHolder extends RecyclerView.ViewHolder {
 
 
     }
+
+
 
     @SuppressLint("SetTextI18n")
     public void onBind(MediaObject mediaObject, RequestManager requestManager) {
