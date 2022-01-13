7ca391f2a850f4b4102e02179ad124b16257e2a1
mdali <paradoxcodee@gmail.com>
2022-01-13 17:17:30 +0530

    9.4

--- app/src/main/java/com/paradox/projectsp3/MyVideoView_Activity.java
+++ app/src/main/java/com/paradox/projectsp3/MyVideoView_Activity.java
@@ -7,26 +7,37 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 public class MyVideoView_Activity extends AppCompatActivity {
 
 
-    Button btn_video;
+    Button btn_video,btn_image;
     VideoView v_video;
     MediaController mc;
+    ImageView imageview;
+    int SELECT_IMAGE_CODE=2;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_my_video_view);
 
 
         btn_video = findViewById(R.id.btn_video);
+        btn_image = findViewById(R.id.btn_image);
         v_video = findViewById(R.id.v_video);
+        imageview = findViewById(R.id.imageview);
 
         mc = new MediaController(MyVideoView_Activity.this);
         v_video.setMediaController(mc);
@@ -38,10 +49,23 @@ public class MyVideoView_Activity extends AppCompatActivity {
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType("video/*");
                 startActivityForResult(intent,1);
+            }
+        });
 
+        btn_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"title"),SELECT_IMAGE_CODE);
 
             }
         });
+
+
     }
 
 
@@ -54,6 +78,11 @@ public class MyVideoView_Activity extends AppCompatActivity {
             v_video.setVisibility(View.VISIBLE);
             v_video.setVideoURI(videouri);
             v_video.start();
+        }
+        if (requestCode == 2){
+            Uri imageuri = data.getData();
+            imageview.setImageURI(imageuri);
+            Toast.makeText(this, String.valueOf(imageuri), Toast.LENGTH_LONG).show();
         }
 
     }
