0ecb22dbc236ebccdf834b409c1037b56bc45c6d
Soosle Infotech <paradoxcodee@gmail.com>
2022-01-13 11:39:24 +0530

    9.2

--- app/src/main/java/com/paradox/projectsp3/Responses/ApiInterface.java
+++ app/src/main/java/com/paradox/projectsp3/Responses/ApiInterface.java
@@ -15,9 +15,9 @@ public interface ApiInterface {
     @GET("get_details")
     Call<Users> performAllPosts();
 
-
-    @POST("upload")
-    Call<JSONArray> postUpload(@Body List<data> dataList);
+//
+//    @POST("upload")
+//    Call<JSONArray> postUpload(@Body List<data> dataList);
 
 
 }
