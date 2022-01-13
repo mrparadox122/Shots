34cdb6bf7bb9136fb903eb71c024721fa76e5265
Soosle Infotech <paradoxcodee@gmail.com>
2022-01-13 11:22:14 +0530

    8.2

--- app/src/main/java/com/paradox/projectsp3/Responses/ApiInterface.java
+++ app/src/main/java/com/paradox/projectsp3/Responses/ApiInterface.java
@@ -1,12 +1,23 @@
 package com.paradox.projectsp3.Responses;
 
+import org.json.JSONArray;
+
+import java.util.List;
+
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface ApiInterface {
 
     ////getting posts///
     @GET("get_details")
     Call<Users> performAllPosts();
+
+
+    @POST("upload")
+    Call<JSONArray> postUpload(@Body List<data> dataList);
+
 
 }
