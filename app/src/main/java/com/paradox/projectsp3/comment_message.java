9d991fe4aa537fff276590fe8a5a31c0dbfa6adf
mohan <mohantarabindra140@gmail.com>
2022-01-20 19:28:57 +0530

    Rabindra(like,comment,share error solve and comment dialog)

---
+++ app/src/main/java/com/paradox/projectsp3/comment_message.java
@@ -0,0 +1,14 @@
+package com.paradox.projectsp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class comment_message extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment_message);
+    }
+}
