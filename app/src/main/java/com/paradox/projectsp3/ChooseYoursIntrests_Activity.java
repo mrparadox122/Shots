45271a293b9e168a8d5621768fdc57d1fd98e3dc
mdali <paradoxcodee@gmail.com>
2022-01-29 18:43:59 +0530

    Prashanth  new Activity(Choose Intereste)

---
+++ app/src/main/java/com/paradox/projectsp3/ChooseYoursIntrests_Activity.java
@@ -0,0 +1,17 @@
+package com.paradox.projectsp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseYoursIntrests_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_yours_intrests);
+
+
+        getSupportActionBar().hide();
+    }
+}
