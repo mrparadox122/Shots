4f73abedfdd9a553766a339bca5c12b622d6e86c
mdali <paradoxcodee@gmail.com>
2022-01-31 10:06:46 +0530

    Prashanth  Project Flow : Splash, ChooseIntrest,SignUp

--- app/src/main/java/com/paradox/projectsp3/MainActivity.java
+++ app/src/main/java/com/paradox/projectsp3/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, NewSignUpActivity.class);
+                Intent intent = new Intent(MainActivity.this, ChooseYoursIntrests_Activity.class);
                 startActivity(intent);
                 finish();
             }
