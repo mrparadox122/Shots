dda1eeea492b0da41cd22e6cc2a0dfcc154ed1a5
mdali <paradoxcodee@gmail.com>
2022-02-07 11:56:30 +0530

    Prashanth changes 07/02/2022

--- app/src/main/java/com/paradox/projectsp3/NewRegister_Activity.java
+++ app/src/main/java/com/paradox/projectsp3/NewRegister_Activity.java
@@ -4,6 +4,9 @@ import static com.google.android.gms.vision.L.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,12 +17,16 @@ import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+import java.util.Calendar;
 
 public class NewRegister_Activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -27,8 +34,12 @@ public class NewRegister_Activity extends AppCompatActivity implements AdapterVi
     Button btn_signup,btn_submit;
 
     String[] Gender = {"Select Gender", "Male", "Female", "Others"};
+    String[] Options = {"","Email", "Phone"};
 
-    boolean isSubmit = true;
+    private Button dateButton;
+    LinearLayout verification;
+    boolean iscode = true;
+    private DatePickerDialog datePickerDialog;
 
 
     @Override
@@ -39,15 +50,20 @@ public class NewRegister_Activity extends AppCompatActivity implements AdapterVi
         getSupportActionBar().hide();
         setContentView(R.layout.activity_new_register);
 
-
         Spinner spinner = findViewById(R.id.spinner);
         spinner.setOnItemSelectedListener(this);
-
 
         ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_spinner_item, Gender);
         ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(ad);
 
+
+        Spinner spinner01 = findViewById(R.id.spinner01);
+        spinner01.setOnItemSelectedListener(this);
+
+        ArrayAdapter add = new ArrayAdapter(this, android.R.layout.simple_spinner_item, Options);
+        add.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner01.setAdapter(add);
 
         et_name = findViewById(R.id.et_name);
         et_Remail = findViewById(R.id.et_Remail);
@@ -56,26 +72,20 @@ public class NewRegister_Activity extends AppCompatActivity implements AdapterVi
         et_RConfirmpassword = findViewById(R.id.et_RConfirmpassword);
         btn_signup = findViewById(R.id.btn_signup);
         btn_submit = findViewById(R.id.btn_submit);
+        verification = findViewById(R.id.verification);
 
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                if (isSubmit == true){
-
-
-                    ////set visivibilty
-
-
+                if (iscode == true){
+                    verification.setVisibility(View.VISIBLE);
+                    Toast.makeText(getApplicationContext(), "Enter Verification Code", Toast.LENGTH_SHORT).show();
                 }else {
-                    Toast.makeText(getApplicationContext(), "sing-up failed", Toast.LENGTH_SHORT).show();
-
+                    verification.setVisibility(View.GONE);
                 }
 
             }
         });
-
-
 
         btn_signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -123,12 +133,100 @@ public class NewRegister_Activity extends AppCompatActivity implements AdapterVi
                 }
             }
         });
+
+
+
+
+        initDatePicker();
+        dateButton = findViewById(R.id.datePickerButton);
+        dateButton.setText(getTodaysDate());
     }
+
+
+
+    private String getTodaysDate()
+    {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+        //datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+
+
+    }
+
+    private String makeDateString(int day, int month, int year)
+    {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month)
+    {
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+
+        //default should never happen
+        return "JAN";
+    }
+
+
+    public void openDatePick(View view)
+    {
+        datePickerDialog.show();
+    }
+
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
-        Toast.makeText(getApplicationContext(), Gender[i], Toast.LENGTH_SHORT).show();
+       // Toast.makeText(getApplicationContext(), Gender[i], Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -136,8 +234,4 @@ public class NewRegister_Activity extends AppCompatActivity implements AdapterVi
 
     }
 
-    public void openDatePicker(View view) {
-
-
-    }
 }
