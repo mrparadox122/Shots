d7e9c805b0c7fbcdb44be7f551cd72770c4e883e
mdali <paradoxcodee@gmail.com>
2022-02-09 17:56:19 +0530

    Prashanth new Calender For DoB and Forgot changes

--- app/src/main/java/com/paradox/projectsp3/ForgotPassword_Activity.java
+++ app/src/main/java/com/paradox/projectsp3/ForgotPassword_Activity.java
@@ -7,11 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class ForgotPassword_Activity extends AppCompatActivity {
 
+    EditText et_mobile;
+    Button send_btn,btn_verify;
+    LinearLayout verification;
 
-
+    boolean send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +26,33 @@ public class ForgotPassword_Activity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_forgot_password);
+
+        send_btn = findViewById(R.id.send_btn);
+        et_mobile = findViewById(R.id.et_mobile);
+        btn_verify = findViewById(R.id.btn_verify);
+
+        send_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (send == true){
+
+                    verification.setVisibility(View.VISIBLE);
+
+                }else {
+
+                    verification.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        btn_verify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ForgotPassword_Activity.this,PhoneNewPinActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
-    public void bt_forget(View view) {
-
-        Intent intent = new Intent(ForgotPassword_Activity.this, Login.class);
-        startActivity(intent);
-    }
 }
