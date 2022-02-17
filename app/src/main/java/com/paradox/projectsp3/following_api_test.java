dd7631c37cfca30bf2dd243d8894810eba8cbb87
md.ali (remote) <alim46622@gmail.com>
2022-02-17 23:00:04 +0530

    paradox (remote) following_api_test (under testing)2

--- app/src/main/java/com/paradox/projectsp3/following_api_test.java
+++ app/src/main/java/com/paradox/projectsp3/following_api_test.java
@@ -1,9 +1,12 @@
 package com.paradox.projectsp3;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.google.android.exoplayer2.extractor.mp4.FixedSampleSizeRechunker;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 public class following_api_test {
     ListView superListView;
@@ -23,18 +26,18 @@ public class following_api_test {
         call.enqueue(new Callback<List<FixedSampleSizeRechunker.Results>>() {
             @Override
             public void onResponse(Call<List<FixedSampleSizeRechunker.Results>> call, Response<List<FixedSampleSizeRechunker.Results>> response) {
-                List<FixedSampleSizeRechunker.Results> myheroList = response.body();
-                String[] one_users = new String[myheroList.size()];
+                List<FixedSampleSizeRechunker.Results> folowing_users_list = response.body();
+                String[] one_users = new String[folowing_users_list.size()];
 
-                for (int i = 0; i < myheroList.size(); i++) {
-                    one_users[i] = myheroList.get(i).getName();
+                for (int i = 0; i < folowing_users_list.size(); i++) {
+                    one_users[i] = folowing_users_list.get(i).getName();
                 }
 
-                superListView.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, oneHeroes));
+                superListView.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, one_users));
             }
 
             @Override
-            public void onFailure(Call<List<Results>> call, Throwable t) {
+            public void onFailure(Call<List<FixedSampleSizeRechunker.Results>> call, Throwable t) {
                 Toast.makeText(getApplicationContext(), "An error has occured", Toast.LENGTH_LONG).show();
             }
 
