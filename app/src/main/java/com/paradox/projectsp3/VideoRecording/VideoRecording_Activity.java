3b2c9507af141b27fd5cd7704c1b404013c2df3a
mdali <paradoxcodee@gmail.com>
2022-03-14 17:52:25 +0530

    Prashanth Working on VideoRecording_Activity

---
+++ app/src/main/java/com/paradox/projectsp3/VideoRecording/VideoRecording_Activity.java
@@ -0,0 +1,19 @@
+package com.paradox.projectsp3.VideoRecording;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.paradox.projectsp3.R;
+
+public class VideoRecording_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_recording);
+
+
+
+    }
+}
