3bb03d4f9f69bdc5615c142cd232d543ef24873d
md.ali (remote) <alim46622@gmail.com>
2022-03-17 23:54:13 +0530

    following video

--- app/src/main/java/com/paradox/projectsp3/HomeFollowing/FollowingActivity.java
+++ app/src/main/java/com/paradox/projectsp3/HomeFollowing/FollowingActivity.java
@@ -4,30 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestManager;
 import com.bumptech.glide.request.RequestOptions;
 import com.paradox.projectsp3.Followers_Following_Likes.FollowersAdapter;
+import com.paradox.projectsp3.GlobalVariables;
 import com.paradox.projectsp3.HomeActivty;
 import com.paradox.projectsp3.HomeRecyclerView.VideoPlayerRecyclerView;
 import com.paradox.projectsp3.MessageMainActivity;
+import com.paradox.projectsp3.Profile.MyVideosScreen_Activity;
+import com.paradox.projectsp3.Profile.P_Commnets;
 import com.paradox.projectsp3.Profile_Activity;
 import com.paradox.projectsp3.R;
+import com.paradox.projectsp3.Responses.ApiInterface;
 import com.paradox.projectsp3.SearchActivity;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class FollowingActivity extends AppCompatActivity {
 
@@ -139,6 +156,186 @@ public class FollowingActivity extends AppCompatActivity {
             setWindowFlag(this,WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,false);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
+
+
+        Retrofit.Builder retrofit1 = new Retrofit.Builder()
+                .baseUrl("http://13.127.217.99/dashboard/paradoxApi/")
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create());
+        Retrofit retrofit3 = retrofit1.build();
+
+
+        //get client
+        ApiInterface apiInterface1 = retrofit3.create(ApiInterface.class);
+        Call<String> calll = apiInterface1.getStringScalar_for_hm(jsonObject1.toString());
+        calll.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                Log.e(ContentValues.TAG, "onResponse: "+response.body() );
+
+
+                if (response.body() != null) {
+                    Log.i("onSuccess", response.body().toString());
+                    String jsonresponse = response.body().toString();
+
+                    try {
+                        //getting the whole json object from the response
+                        JSONObject obj = new JSONObject(jsonresponse);
+                        if (true) {
+
+                            JSONArray dataArray = obj.getJSONArray("body");
+                            for (i = 0; i < dataArray.length(); i++) {
+                                JSONObject dataobj = dataArray.getJSONObject(i);
+                                username.setText(dataobj.getString("fullname"));
+                                Glide.with(getApplicationContext()).load(dataobj.getString("profile_pic")).into(user_pic);
+
+
+
+                            }
+
+                        } else {
+
+
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Toast.makeText(context.getApplicationContext(), "/"+t, Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+
+
+        like_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checklike)
+                {
+                    JSONObject dislike = new JSONObject();
+                    try {
+                        dislike.put("video_id", GlobalVariables.getVideoid());
+                        dislike.put("flag", "1");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    Retrofit.Builder retrofit = new Retrofit.Builder()
+                            .baseUrl("http://13.127.217.99/dashboard/paradoxApi/")
+                            .addConverterFactory(ScalarsConverterFactory.create())
+                            .addConverterFactory(GsonConverterFactory.create());
+                    Retrofit retrofit2 = retrofit.build();
+
+
+                    //get client
+                    ApiInterface apiInterface = retrofit2.create(ApiInterface.class);
+                    Call<String> call_like = apiInterface.getStringScalar(dislike.toString());
+                    call_like.enqueue(new Callback<String>() {
+                        @Override
+                        public void onResponse(Call<String> call, Response<String> response) {
+                            //Toast.makeText(context.getApplicationContext(), "//"+"liked"+response, Toast.LENGTH_SHORT).show();
+                            if (response.isSuccessful()){
+                                likesno+=1;
+                                like_txt.setText(String.valueOf(likesno));
+                            }
+
+
+                        }
+
+                        @Override
+                        public void onFailure(Call<String> call, Throwable t) {
+//                            Toast.makeText(context.getApplicationContext(), "/"+t, Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+                    like_image.setImageResource(R.drawable.ic_icon_material_favorite_red);
+                    checklike=false;
+                }
+                else if (!checklike)
+                {
+                    JSONObject dislike = new JSONObject();
+                    try {
+                        dislike.put("video_id", GlobalVariables.getVideoid());
+                        dislike.put("flag", "4");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    Retrofit.Builder retrofit = new Retrofit.Builder()
+                            .baseUrl("http://13.127.217.99/dashboard/paradoxApi/")
+                            .addConverterFactory(ScalarsConverterFactory.create())
+                            .addConverterFactory(GsonConverterFactory.create());
+                    Retrofit retrofit2 = retrofit.build();
+
+                    //get client
+                    ApiInterface apiInterface = retrofit2.create(ApiInterface.class);
+                    Call<String> call_like = apiInterface.getStringScalar(dislike.toString());
+                    call_like.enqueue(new Callback<String>() {
+                        @Override
+                        public void onResponse(Call<String> call, Response<String> response) {
+                            if (response.isSuccessful()){
+                                likesno-=1;
+                                Toast.makeText(getApplicationContext(), "//"+"disliked"+response, Toast.LENGTH_SHORT).show();
+                                like_txt.setText(String.valueOf(likesno));
+                            }
+
+                        }
+
+                        @Override
+                        public void onFailure(Call<String> call, Throwable t) {
+                            Toast.makeText(getApplicationContext(), "/"+t, Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+
+                    like_image.setImageResource(R.drawable.ic_icon_material_favorite);
+                    checklike=true;
+                }
+            }
+        });
+
+
+        comment_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MyVideosScreen_Activity.this, P_Commnets.class);
+                startActivity(intent);
+
+//                final Dialog dialog = new Dialog(MyVideosScreen_Activity.this);
+//                dialog.setContentView(R.layout.comment_list);
+//                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+//                dialog.getWindow().setGravity(Gravity.BOTTOM);
+//                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//                dialog.show();
+//
+//                recyclerview_cm = findViewById(R.id.recyclerview);
+//                // comment_screen=findViewById(R.id.comment_screen);
+//                cmsend_btn = findViewById(R.id.send_btn);
+//                cmGoback = findViewById(R.id.Goback);
+//
+//                recyclerview_cm.setLayoutManager(new LinearLayoutManager(getApplicationContext(),RecyclerView.VERTICAL,false));
+//                cmcomments_modelList = new ArrayList<>();
+//                cm_adapter = new Comments_Adapter(this,cmcomments_modelList);
+//                recyclerview_cm.setAdapter(cm_adapter);
+            }
+
+        });
 
 
         ///// RecyclerView//////////
