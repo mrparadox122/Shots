285fd49350dc5d1d9e1373974dd0fdd7b2892c70
mdali <paradoxcodee@gmail.com>
2022-03-22 18:07:32 +0530

    Prashanth  video editor done Updated code

---
+++ app/src/main/java/com/paradox/projectsp3/VideoRecording/VideoEditorActivity.java
@@ -0,0 +1,18 @@
+package com.paradox.projectsp3.VideoRecording;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.paradox.projectsp3.R;
+
+public class VideoEditorActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_editor2);
+    }
+}
