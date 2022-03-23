5ee9c90f9e4ca938def5fc1dc4567e6e9223607b
Soosle Infotech <paradoxcodee@gmail.com>
2022-03-23 14:12:40 +0530

    AudioTrimmerActivity done

--- app/src/main/java/com/paradox/projectsp3/SoundsList/SoundActivity.java
+++ app/src/main/java/com/paradox/projectsp3/SoundsList/SoundActivity.java
@@ -6,14 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,7 +25,8 @@ import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
-import com.paradox.projectsp3.HomeActivty;
+import com.paradox.projectsp3.AudioTrimmerActivity;
+import com.paradox.projectsp3.MainActivity;
 import com.paradox.projectsp3.R;
 import com.paradox.projectsp3.VideoEditorFolder.BaseCameraActivity;
 
@@ -34,8 +35,18 @@ import java.util.ArrayList;
 
 public class SoundActivity extends AppCompatActivity {
 
+
+
+    private static final int ADD_AUDIO = 1001;
+    private ImageButton btnAudioTrim;
+    private static final int REQUEST_ID_PERMISSIONS = 1;
+
     ListView sounlst;
+
     String[] itms;
+
+
+
 
 
     @Override
@@ -43,8 +54,12 @@ public class SoundActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound);
         sounlst = (ListView) findViewById(R.id.sounlst);
+
         runtimePremession();
+
+
     }
+
 
     public void runtimePremession() {
         Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
@@ -85,27 +100,46 @@ public class SoundActivity extends AppCompatActivity {
                }
            }
        }
+
+
+
+
+
+
         return arrayList;
+
+
     }
 
 
     public void Displysog(){
         new Thread(new Runnable() { @Override public void run() {
+
             final ArrayList<File> mySongs = findSn(Environment.getExternalStorageDirectory());
             itms = new String[mySongs.size()];
             for (int i=0; i <mySongs.size();i++){
                 itms[i]=mySongs.get(i).getName().toString().replace(".mp3","").replace(".wav","");
             }
             runOnUiThread(new Runnable() {
+
                 @Override
                 public void run() {
+
                     customAdaptor custmAdaptor = new customAdaptor();
                     sounlst.setAdapter(custmAdaptor);
+
                 }
             });
             Log.e(TAG, "run://////////////////////////////////////////////////// " );
 
+
+
+
         } }).start();
+
+
+
+
     }
 
     class customAdaptor extends BaseAdapter{
@@ -129,8 +163,17 @@ public class SoundActivity extends AppCompatActivity {
         public View getView(int i, View Convertview, ViewGroup viewGroup) {
             View view = getLayoutInflater().inflate(R.layout.soundlistitems,null);
             TextView txtSong = view.findViewById(R.id.songttx);
+            btnAudioTrim = view.findViewById(R.id.btnAudioTrim);
             txtSong.setSelected(true);
             txtSong.setText(itms[i]);
+            txtSong.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.e(TAG, "onClick: " );
+                    startActivityForResult(new Intent(SoundActivity.this, AudioTrimmerActivity.class), ADD_AUDIO);
+                    overridePendingTransition(0, 0);
+                }
+            });
             return view;
         }
     }
@@ -142,5 +185,19 @@ public class SoundActivity extends AppCompatActivity {
         startActivity(intent);
         Animatoo.animateSlideDown(SoundActivity.this);
         finish();
+    }
+
+ @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ADD_AUDIO) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+                    //audio trim result will be saved at below path
+                    String path = data.getExtras().getString("INTENT_AUDIO_FILE");
+                    Toast.makeText(SoundActivity.this, "Audio stored at " + path, Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 }
